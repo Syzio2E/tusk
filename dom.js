c74c59c846d44917d0becf60e67276d4699e1432
Shubham saurabh <87587179+Syzio2E@users.noreply.github.com>
2023-03-10 08:01:09 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,14 +1,22 @@
-var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header');
-var font = document.getElementById('title');
-header.style.borderBottom = 'solid 10px #000;';
-title.style.color = 'green';
-title.style.fontweight = 'bold';
+// // var headerTitle = document.getElementById('header-title');
+// // var header = document.getElementById('main-header');
+// // var font = document.getElementById('title');
+// // header.style.borderBottom = 'solid 10px #000;';
+// // title.style.color = 'green';
+// // title.style.fontweight = 'bold';
 
-var li = document.getElementsByClassName('li')
-li[1].style.backgroundColor = 'green';
-li[i].style.fontweight = 'bold'
+// // var li = document.getElementsByClassName('li')
+// // li[1].style.backgroundColor = 'green';
+// // li[i].style.fontweight = 'bold'
 
-for(var i=0;i<items.length;i++){
-    li[i].style.backgroundColor = 'f4f4f4'
-}
+// // for(var i=0;i<items.length;i++){
+// //     li[i].style.backgroundColor = 'f4f4f4'
+// // }
+
+var itemList = document.querySelector('#items');
+// console.log(itemList.parentNode)
+// itemList.parentNode.style.backgroundColor = '#f4f4f4';
+// console.log(itemList.parentElement.parentElement.parentElement)
+
+// console.log(itemList.childNodes);
+console.log(itemList.children)
