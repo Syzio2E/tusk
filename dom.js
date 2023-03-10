6f391b92a9d53a50f8e3cd1210b7ba66bc2593a3
Shubham saurabh <87587179+Syzio2E@users.noreply.github.com>
2023-03-10 07:32:11 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -5,6 +5,10 @@ header.style.borderBottom = 'solid 10px #000;';
 title.style.color = 'green';
 title.style.fontweight = 'bold';
 
-var items = document.getElementsByClassName('list-group-item')
-items[2].style.backgroundColor = 'green';
-items.style.fontweight = 'bold'
+var li = document.getElementsByClassName('li')
+li[1].style.backgroundColor = 'green';
+li[i].style.fontweight = 'bold'
+
+for(var i=0;i<items.length;i++){
+    li[i].style.backgroundColor = 'f4f4f4'
+}
