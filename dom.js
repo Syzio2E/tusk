46f746a1861f5d56f8116cdad796ebaecdcc3818
Shubham saurabh <87587179+Syzio2E@users.noreply.github.com>
2023-03-10 07:18:30 +0530

    Add files via upload

--- dom.js
+++ dom.js
@@ -1,3 +1,10 @@
 var headerTitle = document.getElementById('header-title');
-var header = document.getElementById('main-header')
-header.style.borderBottom = 'solid 10px #000;'
+var header = document.getElementById('main-header');
+var font = document.getElementById('title');
+header.style.borderBottom = 'solid 10px #000;';
+title.style.color = 'green';
+title.style.fontweight = 'bold';
+
+var items = document.getElementsByClassName('list-group-item')
+items[2].style.backgroundColor = 'green';
+items.style.fontweight = 'bold'
